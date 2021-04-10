26c5bc1d87397aa836766424c8d51d4629e993ec
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-04-09 20:24:51 -0600

    allow static file access

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ from flask import request, jsonify, Flask, abort, Response, send_from_directory
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='')
 app.config["DEBUG"] = True
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
-def send_static():
+def home():
     return send_from_directory('static', "index.html")
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 # @app.route('/', methods=['GET'])
 # def home():
