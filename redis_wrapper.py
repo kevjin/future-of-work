2b47ed51df6731489409b9bc538e090b553d7e48
AmyChen-Qingxin <qingxin.chen@vanderbilt.edu>
2021-04-10 00:53:21 -0500

    get last n keys

--- redis_wrapper.py
+++ redis_wrapper.py
@@ -1,11 +1,19 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-
+n=10 #how many record/keys should the db be keeping track of
+last_n_key=f"last{n}keys"
 def set_data(key, value):
     success=r.set(key, value)
+    r.lpush(last_n_key,key)
+    r.ltrim(last_n_key,0,n-1)
     return success
 
 def get_data(key):
     res=r.get(key)
+    return res
+
+def get_last_n(k=n):
+    # k need to <=n. Can be used to specify how many would like to actually retrieve
+    res=r.lrange(last_n_key,0,k-1)
     return res
