f36268ee37f79ef3345f7d388978afb0e91db294
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-04-09 23:48:45 -0600

    production ready

--- server.py
+++ server.py
@@ -1,15 +1,10 @@
-from typing import List
 import flask
 import redis_wrapper
-import sqlite3
 from flask import request, jsonify, Flask, abort, Response, send_from_directory
-from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, static_url_path='')
 app.config["DEBUG"] = True
-cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/dbsuccess')
